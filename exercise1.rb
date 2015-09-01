9d62606d5bdea9a815d4285c2a5edf58deb15a01
michelfrechette <mifrec@hotmail.com>
2015-09-01 15:40:28 -0400

    Simple Ruby program for demostration purposes

---
+++ exercise1.rb
@@ -0,0 +1,9 @@
+2 != 3  # nothing happens when we run $ ruby exercise1.rb
+
+puts 2 != 3  # puts is a Ruby method evaluates to boolean true
+
+puts 2  # a block of Ruby code
+puts 3
+puts 2 != 3
+
+# puts 2 puts 3 puts 2 != 3  # inline code does not work
